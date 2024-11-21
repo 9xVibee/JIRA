eaed897ac2b86be46f469eca70812fb43d359e4c
abhayyy <abhay.panchal@surveysparrow.com>
2024-11-21 19:38:34 +0530

    Customization of buttons done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex">
+      <Button>Primary</Button>
+      <Button variant={'destructive'}>Destructive</Button>
+      <Button variant={'ghost'}>ghost</Button>
+      <Button variant={'muted'}>Muted</Button>
+      <Button variant={'outline'}>outline</Button>
+      <Button variant={'secondary'}>secondary</Button>
+      <Button variant={'teritary'}>Teritary</Button>
+      <Input />
     </div>
   );
 };
